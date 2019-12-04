82a8f8cf99362128e1c2d24ece920be87cb7fda3
unknown <chesdude@gmail.com>
2019-12-04 11:44:02 -0800

    new activity

--- app/src/main/java/edu/uw/stm7631/noteworthy/MainActivity.kt
+++ app/src/main/java/edu/uw/stm7631/noteworthy/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.uw.stm7631.noteworthy
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,81 +12,32 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_courses.*
 
+private lateinit var auth: FirebaseAuth
+
 class MainActivity : AppCompatActivity() {
+    public override fun onStart() {
+        super.onStart()
+        auth = FirebaseAuth.getInstance()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            val intent = Intent(this, SwitchableActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        //setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_courses, R.id.navigation_notes, R.id.navigation_camera
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-
-        var db = FirebaseFirestore.getInstance()
-
-        val docRef = db.collection("users")
-        docRef.get()
-            .addOnSuccessListener { documents ->
-                for (doc in documents) {
-                    CourseContent.ITEMS.add(CourseContent.CourseItem(doc.data["first"].toString(), "hi", "hi"))
-                }
-//                course_recycle.adapter = RecyclerViewAdapter(CourseContent.ITEMS, this)
-//                course_recycle.layoutManager = LinearLayoutManager(this)
-            }
-            .addOnFailureListener { exception ->
-                CourseContent.ITEMS.add(CourseContent.CourseItem("failed", "hi", "hi"))
-//                course_recycle.adapter = RecyclerViewAdapter(CourseContent.ITEMS, this)
-//                course_recycle.layoutManager = LinearLayoutManager(this)
-            }
 
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu to use in the action bar
-        val inflater = menuInflater
-        inflater.inflate(R.menu.example_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        when (item.itemId) {
-            R.id.undo -> {
-                item.title = "Undo"
-                true
-            }
-            R.id.redo -> {
-                item.title = "Redo"
-                true
-            }
-            R.id.find -> {
-                item.title = "Find"
-                true
-            }
-            R.id.settings -> {
-                item.title = "Settings"
-
-
-            }
-            R.id.share -> {
-                item.title = "Share"
-        }}
-                return super.onOptionsItemSelected(item)
-            }
-        }
+}
 
 
 
