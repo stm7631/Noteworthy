9c7721836662018db4fae8f811b6599d6ba0a030
Shane <stm7631@uw.edu>
2019-12-05 00:06:59 -0800

    camera

--- app/src/main/java/edu/uw/stm7631/noteworthy/MainActivity.kt
+++ app/src/main/java/edu/uw/stm7631/noteworthy/MainActivity.kt
@@ -46,13 +46,13 @@ class MainActivity : AppCompatActivity() {
                         if (task.isSuccessful) {
                             //Registration OK
                             val firebaseUser = auth.currentUser
-                            Toast.makeText(this, "signup successful!", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, getResources().getString(R.string.up_success), Toast.LENGTH_SHORT).show()
                             auth.signInWithEmailAndPassword(newemail.text.toString(), newpassword.text.toString())
                             val intent = Intent(this, SwitchableActivity::class.java)
                             startActivity(intent)
                         } else {
                             //Registration error
-                            Toast.makeText(this, "signup failed", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, getResources().getString(R.string.up_fail), Toast.LENGTH_LONG).show()
                         }
                     }
             }
@@ -62,11 +62,12 @@ class MainActivity : AppCompatActivity() {
             signinButton.setOnClickListener {
                 auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener {
                     if (it.isSuccessful) {
+                        Toast.makeText(this, getResources().getString(R.string.in_success), Toast.LENGTH_SHORT).show()
                         val intent = Intent(this, SwitchableActivity::class.java)
                         startActivity(intent)
                     } else {
                         //Registration error
-                        Toast.makeText(this, "signin failed", Toast.LENGTH_SHORT)
+                        Toast.makeText(this, getResources().getString(R.string.in_fail), Toast.LENGTH_LONG).show()
                     }
                 }
             }
